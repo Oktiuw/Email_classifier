805c4a7e263574ba06d852f4dc2c3e8b4787d62b
Aurélien VINCENT <vinau02@gmail.com>
2025-04-09 19:29:32 +0300

    Premier test ok

--- API/main.py
+++ API/main.py
@@ -1,26 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-import pandas as pd
+
+# Charger le modèle et le vectorizer
 model = joblib.load("Model/spam_classifier_model.joblib")
+vectorizer = joblib.load("Model/tfidf_vectorizer.joblib")
+
 app = FastAPI()
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer()
+
 class Email(BaseModel):
     message: str
+
 @app.post("/predict")
 def predict_spam(email: Email):
+    # Transformer le texte en vecteur
     message_vector = vectorizer.transform([email.message])
+    
+    # Prédiction
     prediction = model.predict(message_vector)
+    
     return {"label": prediction[0]}
-new_emails = [
-    "Congratulations! You've won a free vacation. Click here to claim your prize,.",
-    "Meeting reminder: Project update at 2 PM today."
-]
-newDocumentTermMatrix = vectorizer.transform(new_emails)
-predictions = model.predict(newDocumentTermMatrix)
-for email, prediction in zip(new_emails, predictions):
-    print(f"Email: {email}\nPrediction: {prediction}\n")
-message_vector = vectorizer.transform(['test'])
-prediction = model.predict(message_vector)
-print(prediction)
