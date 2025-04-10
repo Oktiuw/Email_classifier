1d450e5254271c223d8c47a1ba578d90adce0567
Aurélien VINCENT <vinau02@gmail.com>
2025-04-10 12:10:54 +0300

    Correction probleme

--- Web_interface/app.py
+++ Web_interface/app.py
@@ -10,12 +10,15 @@ def home():
     prediction = None
     if request.method == "POST":
         email_message = request.form["email_message"]
+        print(f"Message reçu: {email_message}")  # Debug: vérifier le contenu du message
         response = requests.post(API_URL, json={"message": email_message})
         if response.status_code == 200:
             prediction = response.json().get("label", "Erreur dans la réponse de l'API.")
         else:
             prediction = "Erreur avec l'API."
+    print(prediction)
     return render_template("index.html", prediction=prediction)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)  # Serveur sur le port 80
