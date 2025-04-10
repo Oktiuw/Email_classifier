931b3cc209a699b6285c96aaa37ee6ca21951f2c
Aurélien VINCENT <vinau02@gmail.com>
2025-04-10 11:28:47 +0300

    Ajout langue fr backend

--- API/main.py
+++ API/main.py
@@ -7,6 +7,8 @@ import joblib
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from googletrans import Translator
+from langdetect import detect
 
 limiter = Limiter(key_func=get_remote_address)
 MAX_MESSAGE_LENGTH = 5000
@@ -17,6 +19,9 @@ vectorizer = joblib.load("Model/tfidf_vectorizer.joblib")
 app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+translator = Translator()
+
+
 
 class Email(BaseModel):
     message: str
@@ -29,7 +34,10 @@ def clean_input(input_str: str) -> str:
 def predict_spam(email: Email,request: Request):
     if len(email.message) > MAX_MESSAGE_LENGTH:
         raise HTTPException(status_code=400, detail="Message too long. Maximum 5000 characters allowed.")
+    detected_language = detect(email.message)
     message=clean_input(email.message)
+    if detected_language == 'fr':
+        message = translator.translate(message, src='fr', dest='en').text
     message_vector = vectorizer.transform([message])
     prediction = model.predict(message_vector)
     return {"label": prediction[0]}
