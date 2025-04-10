5af3f6d9a273fe630aec1f28e98e580bf1a56c53
Aurélien VINCENT <vinau02@gmail.com>
2025-04-10 11:00:37 +0300

    ajout rate limiter

--- API/main.py
+++ API/main.py
@@ -1,22 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import joblib
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
-# Charger le modèle et le vectorizer
+limiter = Limiter(key_func=get_remote_address)
+
 model = joblib.load("Model/spam_classifier_model.joblib")
 vectorizer = joblib.load("Model/tfidf_vectorizer.joblib")
 
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 class Email(BaseModel):
     message: str
 
 @app.post("/predict")
-def predict_spam(email: Email):
-    # Transformer le texte en vecteur
+@limiter.limit("1/second")  
+def predict_spam(email: Email,request: Request):
     message_vector = vectorizer.transform([email.message])
-    
-    # Prédiction
     prediction = model.predict(message_vector)
     
     return {"label": prediction[0]}
