0ca64ba568c271bcc1cf54bb8af88d176babb123
Aurélien VINCENT <vinau02@gmail.com>
2025-04-10 11:01:59 +0300

    Ajout sécurité requete

--- API/main.py
+++ API/main.py
@@ -1,3 +1,6 @@
+import re
+from html import escape
+
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import joblib
@@ -16,11 +19,14 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 class Email(BaseModel):
     message: str
+def clean_input(input_str: str) -> str:
+    input_str = re.sub(r'[^a-zA-Z0-9\s.,!?\'"]', '', input_str)
+    return escape(input_str)
 
 @app.post("/predict")
 @limiter.limit("1/second")  
 def predict_spam(email: Email,request: Request):
-    message_vector = vectorizer.transform([email.message])
+    message=clean_input(email.message)
+    message_vector = vectorizer.transform([message])
     prediction = model.predict(message_vector)
-    
     return {"label": prediction[0]}
