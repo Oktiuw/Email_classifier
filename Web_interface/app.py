0485d30a3854c7c5987fbe8b849f9a50949fb004
Aurélien VINCENT <vinau02@gmail.com>
2025-04-10 12:37:39 +0300

    AJout .env  et gestion dev/prod

--- Web_interface/app.py
+++ Web_interface/app.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template, request
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
 
 app = Flask(__name__)
 
-API_URL = "http://127.0.0.1:8000/predict" 
+load_dotenv()
+if os.environ.get("FLASK_ENV") == "DEV":
+    API_URL = os.environ.get("API_URL_DEV")
+else:
+    API_URL = os.environ.get("API_URL_PROD")
 
+
+API_URL='http://localhost:8000/predict'
 @app.route("/", methods=["GET", "POST"])
 def home():
     prediction = None
     if request.method == "POST":
         email_message = request.form["email_message"]
-        print(f"Message reçu: {email_message}")  # Debug: vérifier le contenu du message
         response = requests.post(API_URL, json={"message": email_message})
         if response.status_code == 200:
             prediction = response.json().get("label", "Erreur dans la réponse de l'API.")
         else:
             prediction = "Erreur avec l'API."
-    print(prediction)
     return render_template("index.html", prediction=prediction)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)  # Serveur sur le port 80
+    app.run(debug=True, host="0.0.0.0", port=80) 
--- Web_interface/static/js/script.js
+++ Web_interface/static/js/script.js
@@ -22,3 +22,21 @@ function setLanguage(language) {
         document.getElementById('frBtn').style.backgroundColor = '';
     }
 }
+function updateCharCount() {
+    var emailMessage = document.getElementById('emailMessage').value;
+    var charCount = emailMessage.length; 
+    if (charCount <= 5000) {
+        document.getElementById('charCount').innerText = charCount;
+    } else {
+        document.getElementById('emailMessage').value = emailMessage.substring(0, 5000);
+        document.getElementById('charCount').innerText = 5000;
+    }
+    if (charCount < 1) {
+        document.getElementById('classify-btn').disabled = true;
+    } else {
+        document.getElementById('classify-btn').disabled = false;
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    updateCharCount(); 
+});
